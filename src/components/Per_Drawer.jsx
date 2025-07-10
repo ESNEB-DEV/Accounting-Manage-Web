16b818d614fb783f9a0c999f1e7cefb2638b4dd1
ESNEB-DEV <bentzas@gmail.com>
2025-07-10 16:57:27 +0700

    เพิ่มปุ่ม ออกจากระบบ

--- src/components/Per_Drawer.jsx
+++ src/components/Per_Drawer.jsx
@@ -9,21 +9,9 @@ import { Link } from 'react-router-dom';
 import { MdAttachMoney } from "react-icons/md";
 import { FaGear } from "react-icons/fa6";
 import Divider from '@mui/material/Divider';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 
 function Per_Drawer() {
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    
     const drawerWidth = 265;
 
     return (
@@ -35,30 +23,9 @@ function Per_Drawer() {
                     </h2>
                     <div className='flex justify-center items-center space-x-2 text-white'>
                         <FaUserCircle className='w-[20px] h-[20px]' />
-                        <button
-                            id="basic-button"
-                            aria-controls={open ? 'basic-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                            onClick={handleClick}
-                        >
-                            <h1 className='text-md text-white'>ADMINISTRATOR SYSTEM</h1>
-                        </button>
-                        <Menu
-                            id="basic-menu"
-                            anchorEl={anchorEl}
-                            open={open}
-                            onClose={handleClose}
-                            slotProps={{
-                                list: {
-                                    'aria-labelledby': 'basic-button',
-                                },
-                            }} >
-                            <MenuItem onClick={handleClose}>
-                                <a href="/Signin" className="flex items-center block px-3 py-2 rounded text-sm hover:bg-gray-200 hover:text-gray-900">
-                                    <FaSignOutAlt className='mr-2 w-[20px] h-[20px] hover:text-red-600' />ออกจากระบบ</a>
-                            </MenuItem>
-                        </Menu>
+                        <h1 className='text-md text-white'>ADMINISTRATOR SYSTEM</h1>
+                        <a href="/Signin" className="flex items-center block px-3 py-2 rounded text-sm hover:bg-gray-200 hover:text-gray-900">
+                            <FaSignOutAlt className='mr-2 w-[20px] h-[20px]' />ออกจากระบบ</a>
                     </div>
                 </Toolbar>
             </AppBar>
