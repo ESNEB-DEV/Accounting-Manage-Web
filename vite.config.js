7e5394ce6bb0ec59a4436d208097faa9ca15face
ESNEB-DEV <bentzas@gmail.com>
2025-07-11 17:23:43 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/accounting-manage-web/',
   optimizeDeps: {
     include: ['@mui/material', '@mui/icons-material'],
   }
