b93b62088adf86b32bd84c2de2566bb75beaf252
ESNEB-DEV <bentzas@gmail.com>
2025-07-11 17:46:56 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection({
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'indext.html'));
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
 });
 
 // บันทึกการใช้บัตรเครดิต
