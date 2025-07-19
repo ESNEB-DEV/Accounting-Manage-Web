4831b4d2082ac0b36bc45b21dbab8811fb687b34
ESNEB-DEV <bentzas@gmail.com>
2025-07-19 13:14:54 +0700

    deploy connect db test V1

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,19 @@
 const API_URL = import.meta.env.VITE_API_URL;
+const PGHOST = import.meta.env.PGHOST;
+const PGUSER = import.meta.env.PGUSER;
+const PGPASSWORD = import.meta.env.PGPASSWORD;
+const PGDATABASE = import.meta.env.PGDATABASE;
+const PGPORT = import.meta.env.PGPORT;
+const PGSSLMODE = import.meta.env.PGSSLMODE;
+const PGCHANNELBINDING = import.meta.env.PGCHANNELBINDING;
 
 export default {
     API_URL,
+    PGHOST,
+    PGUSER,
+    PGPASSWORD,
+    PGDATABASE,
+    PGPORT,
+    PGSSLMODE,
+    PGCHANNELBINDING,
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Footer from '../components/Footer'
 import Per_Drawer from '../components/Per_Drawer.jsx';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 
 function Dashboard() {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/users') // เรียก API route ที่ deploy อยู่กับ Vercel
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    }, []);
     return (
-        <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-            <Per_Drawer />
-            <Box component="main">
-                <Toolbar />
-                <div className="container mx-auto text-center w-[80rem] h-screen font-NotoSansThai">
-                    <div className='bg-blue-100 p-5 text-left m-5 border rounded'>
-                        <h1 className='text-2xl text-extrabold my-2 text-gray-700'>สวัสดีผู้ใช้</h1>
-                        <p className='text-sm text-gray-600'>ยินดีต้อนรับเข้าสู่ระบบการลงบันทึกรายการค่าใช้จ่าย</p>
-                    </div>
-                </div>
-                <Footer />
-            </Box>
-        </Box>
+        // <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+        //     <Per_Drawer />
+        //     <Box component="main">
+        //         <Toolbar />
+        //         <div className="container mx-auto text-center w-[80rem] h-screen font-NotoSansThai">
+        //             <div className='bg-blue-100 p-5 text-left m-5 border rounded'>
+        //                 <h1 className='text-2xl text-extrabold my-2 text-gray-700'>สวัสดีผู้ใช้</h1>
+        //                 <p className='text-sm text-gray-600'>ยินดีต้อนรับเข้าสู่ระบบการลงบันทึกรายการค่าใช้จ่าย</p>
+        //             </div>
+        //         </div>
+        //         <Footer />
+        //     </Box>
+        // </Box>
+        <div>
+            <h1>รายชื่อผู้ใช้</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
 
     )
 }
